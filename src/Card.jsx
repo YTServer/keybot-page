7f258b2de548ec4553d68165739c7a889450436a
Whitey <c1783468@gmail.com>
2023-03-05 19:59:14 +0800

    fix home page rwd

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,17 +6,17 @@ export class Card extends React.Component {
   }
   render() {
     return (
-      <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md w-1/3">
-        <div className="rounded-2xl bg-sky-900 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-          <div className="mx-auto max-w-xs px-8">
+      <div className="-mt-2 w-1/3 p-2 lg:mt-0 lg:w-full lg:max-w-md">
+        <div className="rounded-2xl bg-sky-900 py-4 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+          <div className="mx-auto max-w-xs sm:px-4 lg:px-8">
             <p className="text-base font-semibold text-white">
               {this.props?.names}
             </p>
             <p className="mt-6 flex items-baseline justify-center gap-x-2">
-              <span className="text-5xl font-bold tracking-tight text-lime-300">
+              <span className="font-bold tracking-tight text-lime-300 sm:text-xl lg:text-5xl">
                 {this.props?.number}
               </span>
-              <span className="text-sm font-semibold leading-6 tracking-wide text-lime-300">
+              <span className="font-semibold leading-6 tracking-wide text-lime-300 sm:text-xs lg:text-sm">
                 {this.props?.unit}
               </span>
             </p>
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,22 +7,24 @@ export class HomePage extends React.Component {
     return (
       <div id="home">
         <div className="container mx-auto mt-28 flex max-w-7xl items-center justify-between p-6 lg:px-8">
-          <div className="basis-1/3">
-            <img className="min-w-full aspect-square" src={keyBot}></img>
+          <div className="max-sm:hidden md:basis-1/3">
+            <img className="aspect-square min-w-full" src={keyBot}></img>
           </div>
-          <div className="basis-2/3 pl-40 text-left text-white">
-            <p className="text-4xl">Team Fortress 2 Keys </p>
+          <div className="text-white md:basis-2/3 lg:pl-40">
+            <p className="sm:text-xl md:text-2xl lg:text-4xl ">
+              Team Fortress 2 Keys{" "}
+            </p>
             <p className="pt-10">以最優惠的方式獲取 TF2 鑰匙</p>
             <p className="pb-10">The most favorable way to buy TF2 keys</p>
             <a
               href="steam://friends/add/76561198047686623"
-              className="rounded-md bg-lime-300 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="rounded-md bg-lime-300 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white max-lg:hidden"
             >
               加好友
             </a>
           </div>
         </div>
-        <div className="flex flex-row items-center justify-center p-6 lg:px-8 max-w-full">
+        <div className="flex max-w-full flex-row items-center justify-center sm:p-4 lg:p-6 lg:px-8">
           <Card names="售價" number="55" unit="NTD" />
           <Card names="庫存" number="100" unit="把" />
           <Card names="已完成交易" number="1000000" unit="筆" />

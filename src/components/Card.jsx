f702b7efad9a2bea01e286f4c138f1449febc88d
Whitey <c1783468@gmail.com>
2023-04-23 13:55:22 +0800

    new home section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,24 +13,14 @@ export class Card extends React.Component {
         <img src={loading} alt="loading icon" />
       </div>
     );
-    const number = (
-      <span className="font-bold tracking-tight text-lime-300 sm:text-xl lg:text-5xl">
-        {this.props?.number}
-      </span>
-    );
     return (
-      <div className="-mt-2 w-1/3 p-2 lg:mt-0 lg:w-full lg:max-w-md">
-        <div className="rounded-2xl bg-sky-900 py-4 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-          <div className="mx-auto max-w-xs sm:px-4 lg:px-8">
-            <p className="text-base font-semibold text-white">
-              {this.props?.names}
-            </p>
-            <div className="mt-6 flex items-baseline justify-center gap-x-2">
-              {this.props.loading ? spinner : number}
-              <span className="font-semibold leading-6 tracking-wide text-lime-300 sm:text-xs lg:text-sm">
-                {this.props?.unit}
-              </span>
-            </div>
+      <div className="w-full p-2">
+        <div className="rounded-2xl bg-slate-700 py-4 text-center ring-1 ring-inset ring-gray-900/5">
+          <div className="flex flex-col items-center justify-center">
+            <dt className="mb-2 text-3xl font-extrabold text-white">
+              {this.props.loading ? spinner : this.props?.number}
+            </dt>
+            <dd className="text-gray-400">{this.props?.names}</dd>
           </div>
         </div>
       </div>
@@ -41,6 +31,5 @@ export class Card extends React.Component {
 Card.propTypes = {
   names: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired,
-  unit: PropTypes.string.isRequired,
   loading: PropTypes.bool.isRequired,
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,44 +34,44 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div id="home" className="">
-        <div className="container mx-auto mt-28 flex max-w-7xl items-center justify-between">
-          <div className="max-sm:hidden md:basis-1/3">
-            <img className="aspect-square min-w-full" src={keyBot}></img>
+        <section className="mt-28 bg-gray-900">
+          <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl">
+                Whitey&apos;s TF2 Key Bot
+              </h1>
+              <p className="mb-6 max-w-2xl font-light text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+                以最優惠的方式獲取 TF2 鑰匙
+              </p>
+              <a
+                href="steam://friends/add/76561198047686623"
+                className="mr-2 mb-2 rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800 max-sm:hidden"
+              >
+                新增好友
+              </a>
+              <div className="mt-4 flex max-w-full flex-row items-center justify-center">
+                <Card
+                  names="售價"
+                  loading={this.state.loading}
+                  number={this.state.price}
+                />
+                <Card
+                  names="庫存"
+                  loading={this.state.loading}
+                  number={this.state.stock}
+                />
+                <Card
+                  names="成交量"
+                  loading={this.state.loading}
+                  number={this.state.orders}
+                />
+              </div>
+            </div>
+            <div className="hidden lg:col-span-5 lg:mt-0 lg:flex">
+              <img src={keyBot} alt="key" />
+            </div>
           </div>
-          <div className="text-white md:basis-2/3 lg:pl-40">
-            <p className="sm:text-xl md:text-2xl lg:text-4xl ">
-              Team Fortress 2 Keys
-            </p>
-            <p className="pt-10">以最優惠的方式獲取 TF2 鑰匙</p>
-            <p className="pb-10">The most favorable way to buy TF2 keys</p>
-            <a
-              href="steam://friends/add/76561198047686623"
-              className="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-lime-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white max-sm:hidden"
-            >
-              加好友
-            </a>
-          </div>
-        </div>
-        <div className="flex max-w-full flex-row items-center justify-center">
-          <Card
-            names="售價"
-            loading={this.state.loading}
-            number={this.state.price}
-            unit="NTD"
-          />
-          <Card
-            names="庫存"
-            loading={this.state.loading}
-            number={this.state.stock}
-            unit="把"
-          />
-          <Card
-            names="已完成交易"
-            loading={this.state.loading}
-            number={this.state.orders}
-            unit="筆"
-          />
-        </div>
+        </section>
       </div>
     );
   }

749f06c918c13a81c86c28d8aa7c3db6a47b20be
Whitey <c1783468@gmail.com>
2023-04-25 02:11:53 +0800

    add ga

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-KXXVTV8V14');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 import store from './models/store';
 import { Provider } from 'react-redux';

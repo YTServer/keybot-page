e91a103535333626bd3fc6199e05d8af4d863ac4
Whitey <c1783468@gmail.com>
2023-04-24 20:35:46 +0800

    new buy process

--- src/components/BuyProcessCard.jsx
+++ src/components/BuyProcessCard.jsx
@@ -5,68 +5,62 @@ export default class BuyProcessCard extends React.Component {
     super(props);
   }
 
+  arrowSvg = (
+    <svg
+      aria-hidden="true"
+      className="ml-2 hidden h-4 w-4 sm:ml-4 md:block"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M13 5l7 7-7 7M5 5l7 7-7 7"
+      ></path>
+    </svg>
+  );
+
+  steps = [
+    {
+      title: '新增好友',
+      details: '',
+    },
+    {
+      title: '產生訂單',
+      details: '使用 buy 指令',
+    },
+    {
+      title: '繳費',
+      details: '使用代碼至超商繳費',
+    },
+    {
+      title: '取貨',
+      details: '發交易提案給 Bot',
+    },
+  ];
+
   render() {
     return (
-      <div className="mx-auto flex max-w-7xl place-items-center justify-between">
-        <div className="relative w-1/4 p-5 lg:mt-0">
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-sky-500 text-center">
-              1
-            </div>
-          </div>
-          <div className="rounded-2xl bg-sky-900 py-4 ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:py-8">
-            <div className="mx-auto  max-w-xs text-white sm:px-4 lg:px-8">
-              先加
-              <br></br>
-              <a
-                href="steam://friends/add/76561198047686623"
-                className="rounded-md bg-lime-500 px-1 py-1 text-sm font-semibold text-gray-900 shadow-sm hover:bg-lime-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                √Whitey | TF2 Keys Bot{' '}
-              </a>
-              <br></br>
-              好友輸入 buy
-            </div>
-          </div>
-        </div>
-        <div className="relative w-1/4 p-5 lg:mt-0">
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-sky-500 text-center">
-              2
-            </div>
-          </div>
-          <div className="rounded-2xl bg-sky-900 py-4 ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:py-8">
-            <div className="mx-auto  max-w-xs text-white sm:px-4 lg:px-8">
-              指令產生新訂單<br></br>例如：輸入 buy 10，會產生一筆 10 keys
-              的訂單
-            </div>
-          </div>
-        </div>
-        <div className="relative w-1/4 p-5 lg:mt-0">
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-sky-500 text-center">
-              3
-            </div>
-          </div>
-          <div className="rounded-2xl bg-sky-900 py-4 ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:py-8">
-            <div className="mx-auto  max-w-xs text-white sm:px-4 lg:px-8">
-              確認訂單數量、金額正確後到超商繳費
-            </div>
-          </div>
-        </div>
-        <div className="relative w-1/4 p-5 lg:mt-0">
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-center">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-sky-500 text-center">
-              4
-            </div>
-          </div>
-          <div className="rounded-2xl bg-sky-900 py-4 ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:py-8">
-            <div className="mx-auto  max-w-xs text-white sm:px-4 lg:px-8">
-              完成繳費後，傳送交易提案給 Bot 取貨
-            </div>
-          </div>
-        </div>
-      </div>
+      <ol className="w-full items-center justify-center space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+        {this.steps.map((step, index) => (
+          <li
+            className="flex items-center space-x-2.5 text-blue-500"
+            key={index}
+          >
+            <span className="flex h-8 w-8 shrink-0 items-center justify-center rounded-full border border-blue-500">
+              {index + 1}
+            </span>
+            <span>
+              <h3 className="font-bold leading-tight">{step.title}</h3>
+              <p className="text-sm">{step.details}</p>
+            </span>
+            {index < this.steps.length - 1 ? this.arrowSvg : null}
+          </li>
+        ))}
+      </ol>
     );
   }
 }
